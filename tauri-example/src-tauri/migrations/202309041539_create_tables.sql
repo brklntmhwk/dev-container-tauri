b263b974e04f2233b340d809f5bd6a9bc5547c51
brklntmhwk <to.be.wanryu@gmail.com>
2023-09-04 09:03:15 +0000

    Added some packages to package.json and Cargo.toml

---
+++ tauri-example/src-tauri/migrations/202309041539_create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE notes
+(
+  note_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  content TEXT NOT NULL,
+  created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
+
+CREATE UNIQUE INDEX unique_notes_index ON notes (title);
--- tauri-example/src-tauri/src/main.rs
+++ tauri-example/src-tauri/src/main.rs
@@ -1,15 +1,54 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
+// use sqlx::postgres::{PgPool, PgPoolOptions};
+use tauri::plugin::TauriPlugin;
+use tauri::{CustomMenuItem, Manager, Menu, MenuItem, Runtime};
+use tauri_plugin_sql::{Builder as TauriSqlBuilder, Migration, MigrationKind, PluginConfig};
+use tauri_plugin_store::Builder as TauriStoreBuilder;
+
+#[derive(Serialize)]
+struct Data {
+    id: i32,
+    content: String,
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// #[tauri::command]
+// async fn handle_add_note() -> Result<()> {
+//     Ok(())
+// }
+
 fn main() {
     tauri::Builder::default()
+        .plugin(enable_sql_plugin())
+        .plugin(enable_store_plugin())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn enable_sql_plugin<R: Runtime>() -> TauriPlugin<R, Option<PluginConfig>> {
+    TauriSqlBuilder::default()
+        .add_migrations(
+            "postgres://postgres:postgres@localhost:5432/db",
+            vec![Migration {
+                version: 1,
+                description: "Create tables",
+                sql: include_str!("../migrations/202309041539_create_tables.sql"),
+                kind: MigrationKind::Up,
+            }],
+        )
+        .build()
+}
+
+fn enable_store_plugin<R: Runtime>() -> TauriPlugin<R> {
+    TauriStoreBuilder::default().build()
 }
